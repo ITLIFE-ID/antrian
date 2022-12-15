7c8236350ab989f9b4ede3fb84829a8f168319b4
NW00039 <helmi.susanto@otoraja.co.id>
2022-12-15 22:49:59 +0700

    change to each with index

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -3,6 +3,6 @@
 module ApplicationHelper
   def days_of_week
     days = Date::DAYNAMES.rotate(1)
-    days.while_loop.map { |value, index| {(index + 1).to_s => value} }.reduce({}, :merge)
+    days.each_with_index.map { |value, index| {(index + 1).to_s => value} }.reduce({}, :merge)
   end
 end
