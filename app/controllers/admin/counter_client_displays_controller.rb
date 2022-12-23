8be6f3c1c053129e652f7e4e1283021bd2581b72
NW00039 <helmi.susanto@otoraja.co.id>
2022-12-23 21:27:26 +0700

    fix label

--- app/controllers/admin/counter_client_displays_controller.rb
+++ app/controllers/admin/counter_client_displays_controller.rb
@@ -1,6 +1,6 @@
 module Admin
   class CounterClientDisplaysController < Admin::ApplicationController
-    add_breadcrumb I18n.t("counter_clientdisplay")
+  add_breadcrumb I18n.t("counter_clientdisplay")
     # Overwrite any of the RESTful controller actions to implement custom behavior
     # For example, you may want to send an email after a foo is updated.
     #
