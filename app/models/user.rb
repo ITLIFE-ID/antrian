b9855e69c658741f0a882b3a66973770ce188769
NW00039 <helmi.susanto@otoraja.co.id>
2022-12-27 00:31:50 +0700

    check scope

--- app/models/user.rb
+++ app/models/user.rb
@@ -55,6 +55,7 @@
 class User < ApplicationRecord
   include Permisi::Actable
   has_many :user_counters
+  belongs_to :company
 
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
