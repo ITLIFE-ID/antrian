ad1951e4b1e6bab8630cb62552aef9f866f5bf95
“helmiatwork” <helmi.at.work@gmail.com>
2023-01-08 01:32:58 +0700

    remove recall

--- app/services/queue_service.rb
+++ app/services/queue_service.rb
@@ -120,7 +120,7 @@ class QueueService < ApplicationService
       <td>#{queue_number}<span class='ml-2 badge #{badge}'>#{x.print_ticket_method}</span></td>
       <td>#{priority}</td>
       <td>
-        <button type='button' class='btn btn-info font-weight-bolder w-100 recall' data-id='#{x.id}'>Panggil Ulang</button>
+        <button type='button' class='btn btn-info font-weight-bolder w-100'>Panggil Ulang</button>
       </td>
     </tr>"
     end
