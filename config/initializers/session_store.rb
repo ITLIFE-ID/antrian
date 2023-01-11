c1e82670e59371224c687b0499d201e6f7c729bf
“helmiatwork” <helmi.at.work@gmail.com>
2023-01-12 01:34:31 +0700

    fix session stores

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,5 +1,3 @@
-ActionDispatch::Session::ActiveRecordStore.session_class = ActiveRecord::SessionStore::CustomSession
-
 if ENV["REDIS_URL"].present?
   Rails.application.config.session_store :redis_store, servers: ["#{ENV["REDIS_URL"]}/0/queue_system_#{Rails.env}_session"], expire_after: 24.hours    
 end
--- lib/add_on/active_record/session_store/custom_session.rb
+++
@@ -1,9 +0,0 @@
-module ActiveRecord
-  module SessionStore
-    class CustomSession < Session
-      def save
-        super
-      end
-    end
-  end
-end
