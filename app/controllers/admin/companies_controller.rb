5e4bfa2cb31869bceb66153a7ee0cd18f07ff1ed
“helmiatwork” <helmi.at.work@gmail.com>
2023-01-12 09:22:41 +0700

    fix company scope

--- app/controllers/admin/companies_controller.rb
+++ app/controllers/admin/companies_controller.rb
@@ -3,7 +3,22 @@ module Admin
     add_breadcrumb I18n.t("company")
     # Overwrite any of the RESTful controller actions to implement custom behavior
     # For example, you may want to send an email after a foo is updated.
-    #
+    
+    def create
+      resource = scoped_resource.new(resource_params)
+
+      if resource.save
+        redirect_to(
+          after_resource_created_path(resource),
+          notice: translate_with_resource("create.success")
+        )
+      else
+        render :new, locals: {
+          page: Administrate::Page::Form.new(dashboard, resource)
+        }, status: :unprocessable_entity
+      end
+    end
+    
     # def update
     #   super
     #   send_foo_updated_email(requested_resource)
